933b83d36f3f9e8d86306aa99b7b62a480412208
Avery Miller <averymiller2060@gmail.com>
2025-01-17 12:48:01 -0500

    completed equal sum checker challenge

---
+++ Section5CodingExercises/EqualSumChecker.java
@@ -0,0 +1,13 @@
+package Section5CodingExercises;
+
+public class EqualSumChecker {
+  public static void main(String[] args) {
+    
+  }
+
+  public static boolean hasEqualSum(int num1, int num2, int num3) {
+    int sum = num1 + num2;
+    return (sum == num3);
+  }
+
+}
