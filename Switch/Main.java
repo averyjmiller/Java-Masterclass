a62b30602b09603170d54939566a294be6301198
Avery Miller <averymiller2060@gmail.com>
2025-01-20 20:58:08 -0500

    added enhanced switch statement example

--- Switch/Main.java
+++ Switch/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
     int switchValue = 4;
 
+    // Traditional Switch Statement
     switch (switchValue) {
       case 1:
         System.out.println("Value was 1");
@@ -28,5 +29,33 @@ public class Main {
       default:
         System.out.println("Was not 1, 2, 3, 4, or 5");
     }
+
+    // Enhanced Switch Statement
+    switch (switchValue) {
+      case 1 -> System.out.println("Value was 1");
+      case 2 -> System.out.println("Value was 2");
+      case 3, 4, 5 -> {
+        System.out.println("Was a 3, a 4, or a 5");
+        System.out.println("Actually it was a " + switchValue);
+      }
+      default -> System.out.println("Was not 1, 2, 3, 4, or 5");
+    }
+
+    String month = "APRIL";
+    System.out.println(month + " is in the " + getQuarter(month) + " quarter");
   }
+
+  public static String getQuarter(String month) {
+    return switch (month) {
+      case "JANUARY", "FEBRUARY", "MARCH" -> { yield "1st"; }
+      case "APRIL", "MAY", "JUNE" -> "2nd";
+      case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+      case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+      default -> {
+        String badResponse = month + " is bad";
+        yield badResponse;
+      }
+    };
+  }
+
 }
