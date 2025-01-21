81868f170f97f267de63f50d8ac248a308c5777f
Avery Miller <averymiller2060@gmail.com>
2025-01-20 21:06:04 -0500

    completed traditional switch challenge

---
+++ Switch/TraditionalSwitchChallenge.java
@@ -0,0 +1,28 @@
+package Switch;
+
+public class TraditionalSwitchChallenge {
+  public static void main(String[] args) {
+
+    char character = 'B';
+    switch (character) {
+      case 'A':
+        System.out.println("A = Able");
+        break;
+      case 'B':
+        System.out.println("B = Baker");
+        break;
+      case 'C':
+        System.out.println("C = Charlie");
+        break;
+      case 'D':
+        System.out.println("D = Dog");
+        break;
+      case 'E':
+        System.out.println("E = Easy");
+        break;
+      default:
+        System.out.println("Not found");
+        break;
+    }
+  }
+}
