29b146ea4ecf76d6f21954d23fbf53850712aba1
Avery Miller <averymiller2060@gmail.com>
2025-02-09 13:54:08 -0500

    created example of class

---
+++ ClassesPart1/Main.java
@@ -0,0 +1,13 @@
+package ClassesPart1;
+
+public class Main {
+  public static void main(String[] args) {
+    Car car = new Car();
+    car.setMake("Porsche");
+    car.setModel("Carrera");
+    car.setDoors(2);
+    car.setConvertible(true);
+    car.setColor("black");
+    car.describeCar();
+  }
+}
