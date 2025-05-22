b59d94eca6be309e52806f589925138fa334c885
Avery <averymiller2060@gmail.com>
2025-05-21 20:04:05 -0400

    completed Customer class challenge using constructors

---
+++ ClassesChallenge/Customer.java
@@ -0,0 +1,37 @@
+package ClassesChallenge;
+
+public class Customer {
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    // Default constructor
+    public Customer() {
+        name = "John";
+        creditLimit = 5000;
+        email = "johndoe@gmail.com";
+    }
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    // Constructor chaining
+    public Customer(String name, String email) {
+        this(name, 1000, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
